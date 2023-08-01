03e91826758ee15a1d328aa938286d8c40436f05
brilliswift <113734241+brilliswift@users.noreply.github.com>
2023-08-01 23:24:03 +0700

    Add files via upload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('Dompdf_gen', 'database', 'session', 'upload', 'TCPDF_gen');
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array();
+$autoload['helper'] = array('application', 'url', 'file', 'security', 'date', 'captcha', 'form', 'language');
 
 /*
 | -------------------------------------------------------------------
